f4a78da1b954594c0a04fa6afa90ea49878a7042
zxx <yahone.chow@gmail.com>
2021-04-27 16:05:57 +0800

    ###

--- paint-board.js
+++ paint-board.js
@@ -5,14 +5,17 @@
 
 class PaintBoard{
     props = null;
-    _painting = false;
+    isPainting = false;
+    isClean = true;
+    isContinuous = false;
+    _lastMouseUpTimeStamp = null;
     strokeConfig = {
         strokeWidth : 1,
         strokeColor : '#000000'
     };
 
     constructor(props){
-        let { canvas, width, height, inputEventFunc, ...rest } = props;
+        let { canvas, width, height, inputEventFunc, history = false, historyMax = 10 } = props;
 
         canvas.width = width;
         canvas.height = height;
@@ -22,10 +25,24 @@ class PaintBoard{
         this.ctx = canvas.getContext('2d');
 
         this.Init();
+
+        if(history){
+            this.history = history;
+            this.historyMax = history;
+            this.historyIndex = 0;
+            this.historyStack = [
+                {
+                    t : 'init'
+                }
+            ];
+        }
     }
 
     Init(){
         let { canvas, ctx } = this;
+        const disabledSelection = (event) => {
+            event.preventDefault();
+        };
         const move = (event) => {
             let { strokeWidth, strokeColor } = this.strokeConfig;
             let currCoord = PaintBoard.CoordTransform({
@@ -34,27 +51,44 @@ class PaintBoard{
             });
 
             PaintBoard.Stroke({
-                lastCoord : this._lastCoord,
+                lastCoord : this.lastCoord,
                 currCoord,
                 ctx,
                 strokeWidth,
                 strokeColor
             });
 
-            this._lastCoord = currCoord;
+            this.lastCoord = currCoord;
+        };
+        const up = () => {
+            this.isPainting = false;
+
+            if(this.history){
+                this.Snapshot(this.isContinuous);
+                this._lastMouseUpTimeStamp = Date.now();
+            }
+            document.removeEventListener('mousemove', move);
+            document.removeEventListener('selectstart', disabledSelection);
+            document.removeEventListener('mouseup', up);
         };
 
         this.canvas.onmousedown = (event) => {
-            this._painting = true;
-            this._lastCoord = PaintBoard.CoordTransform({
+            this.isPainting = true;
+            this.lastCoord = PaintBoard.CoordTransform({
                 canvas,
                 event
             });
+
+            if(this.history){
+                this.isClean = false;
+                this.ClearRedoList();
+                // two operates interval less than 500ms
+                this.isContinuous = (Date.now() - this._lastMouseUpTimeStamp) < 1000;
+            }
+
             document.addEventListener('mousemove', move);
-            document.addEventListener('mouseup', () => {
-                this._painting = false;
-                document.removeEventListener('mousemove', move);
-            });
+            document.addEventListener('selectstart', disabledSelection);
+            document.addEventListener('mouseup', up);
         };
     }
 
@@ -66,12 +100,92 @@ class PaintBoard{
 
     }
 
+    ToggleErase(){
+    }
+
+    Snapshot(replace = false){
+        let historyItem = null;
+        // cleared
+        console.log('Snapshot - isClean - ', this.isClean);
+        if(this.isClean){
+            historyItem = {
+                t : 'clear'
+            };
+
+        } else{
+            historyItem = {
+                time : Date.now(),
+                data : this.canvas.getContext('2d')
+                           .getImageData(0, 0, this.canvas.width, this.canvas.height).data
+            };
+        }
+
+        // replace
+        if(replace){
+            this.historyStack.pop();
+        }
+
+        // handle maximum to avoids overflow
+        if(this.historyStack.length + 1 > this.historyMax){
+            this.historyStack.unshift();
+        }
+
+        this.historyStack.push(historyItem);
+        this.historyIndex = this.historyStack.length - 1;
+        console.log(this.historyStack, this.historyIndex);
+    }
+
+    ApplyHistory(){
+        let handleData = this.historyStack[this.historyIndex];
+
+        if(handleData){
+            if(handleData.t === 'init' || handleData.t === 'clear'){
+                this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+                this.isClean = true;
+            } else{
+                let imageData = new ImageData(handleData.data, this.canvas.width, this.canvas.height);
+                this.ctx.putImageData(imageData, 0, 0);
+            }
+        }
+    }
+
+    // New operations will clear
+    ClearRedoList(){
+        this.historyStack.length = this.historyIndex + 1;
+    }
+
+    Undo(){
+        this.historyIndex -= 1;
+
+        if(this.historyIndex < 0){
+            this.historyIndex = 0;
+        }
+
+        this.ApplyHistory();
+    }
+
+    Redo(){
+        this.historyIndex += 1;
+
+        if(this.historyIndex > this.historyStack.length - 1){
+            this.historyIndex = this.historyStack.length - 1;
+        } else{
+            this.ApplyHistory();
+        }
+    }
+
+    Clear(){
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.isClean = true;
+        this.ClearRedoList();
+        this.Snapshot(this.historyStack[this.historyIndex].t === 'clear');
+    }
+
     static Stroke({ ctx, lastCoord, currCoord, strokeWidth, strokeColor }){
         ctx.beginPath();
         ctx.lineCap = 'round';
         ctx.lineWidth = strokeWidth;
         ctx.strokeStyle = strokeColor;
-        console.log(lastCoord, currCoord);
         ctx.moveTo(lastCoord.x, lastCoord.y);
         ctx.lineTo(currCoord.x, currCoord.y);
         ctx.stroke();
