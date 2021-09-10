a1f4ae0b57a803e281d0799d26f4d1f01f34e530
zxx <yahone.chow@gmail.com>
2021-09-10 11:07:00 +0800

    Update webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,10 @@
-const webpack = require('webpack');
+// const webpack = require('webpack');
 // const WebpackCleanPlugin = require('clean-webpack-plugin');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.base.js');
 
-module.exports = merge(common,
+module.exports = merge(
+    common,
     {
         mode : 'production'
     }
